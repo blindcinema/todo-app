692e0ef832c6f0fd8d14ca57b85f88e9b693bb0c
Jan <jannemetguzvic@gmail.com>
2023-03-31 18:44:09 +0200

    mark and list

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,17 @@ import { useState } from 'react'
 function App() {
   const [items, setItems] = useState(defaultItems);
   const itemComponents = items.map( item => {
+    const handleChange = () => {
+      console.log("handle change for item", item);
+      setItems(items.map(newItem => {
+        if (newItem.id === item.id) {
+          return  { ...newItem, done: !item.done};
+        }
+        else return newItem;
+      }));
+    };
     return ( 
-      <div key={item.id}><input type="checkbox" checked={item.done} />{item.text}</div>
+      <div key={item.id}><input type="checkbox" checked={item.done} onChange={handleChange} />{item.text}</div>
     );
   });
 
