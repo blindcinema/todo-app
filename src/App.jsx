487dfb7cd9343f12907a732125f9a6bfa36f4bd6
Jan <jannemetguzvic@gmail.com>
2023-03-31 19:43:49 +0200

    fixed stuff + checked

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,59 @@ import { useState } from 'react'
 
 function App() {
   const [items, setItems] = useState(defaultItems);
+  const [formState, setFormState] = useState("");
   const itemComponents = items.map( item => {
-    const handleChange = () => {
+    const handleChange = (event) => {
+
       console.log("handle change for item", item);
       setItems(items.map(newItem => {
+
         if (newItem.id === item.id) {
-          return  { ...newItem, done: !item.done};
+
+          return(  { ...newItem, done: !item.done});
         }
-        else return newItem;
+        else {
+         return newItem;
+        }
       }));
     };
+    const handleClick = () => {
+      setItems(items.filter(newItem => {
+        return newItem.id !== item.id;
+      }));
+    };
+
+
+
     return ( 
-      <div key={item.id}><input type="checkbox" checked={item.done} onChange={handleChange} />{item.text}</div>
+      <div key={item.id} style={{ textDecoration: item.done ? "line-through" : "none"}}><input type="checkbox" checked={item.done} onChange={handleChange} />{item.text}
+      <button onClick={handleClick}>x</button></div>
     );
   });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormState("");
+    setItems([ ...items,
+       {id: Date.now(),
+        text: formState,
+        done: false,
+
+    }]  );
+  }
+
+
+  function handleInputChange(event) {
+    setFormState(event.target.value);
+  }
 
 
   return (
     <div className="App">
       <h1>TODO APP</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInputChange} value={formState} />
+        <button type="submit">Add</button>
+      </form>
       {itemComponents}
     </div>
   )
